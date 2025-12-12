ffd6d373dcd09ec437444b12225c1c1c8452315b
izyranov154-png <izyranov154@gmail.com>
2025-12-12 20:25:43 +0300

    Добавил 2 счетчика для игроков

--- ping_pong.py
+++ ping_pong.py
@@ -1,9 +1,13 @@
 from pygame import *
 FPS = 60
 
+font.init()
+font = font.Font(None, 40)
+
 clock = time.Clock()
 window = display.set_mode((600,400))
-
+score1 = 0
+score2 = 0
 
 #Классы
 class GameSprite(sprite.Sprite):
@@ -16,6 +20,10 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        
+        self.speed_x = self.speed
+        self.speed_y = self.speed 
+        
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -33,13 +41,8 @@ class Player(GameSprite):
         if key_pressed[K_d] and self.rect.y < 250:
             self.rect.y += self.speed
 class Ball(GameSprite):
-    def __init__(self, player_image,player_x,player_y,player_speed, player_width,player_height):
-        super().__init__(player_image,player_x, player_y, player_speed,player_width, player_height)
 
-        self.speed_x = self.speed
-        self.speed_y = self.speed
     def ball_update(self):
-
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
         
@@ -50,11 +53,12 @@ class Ball(GameSprite):
             self.speed_x *= -1
         
         if sprite.collide_rect(player1, ball):
+
             self.speed_x *= -1
 
 player1 = Player('raket1.png',20, 120,6,50,150)
 player2 = Player('raket1.png',530, 120,6,50,150)
-ball = Ball('ball1.png',100,150,6,40,40)
+ball = Ball('ball1.png', 280, 180, 4, 40, 40)
 game = True
 while game:
     for e in event.get():
@@ -71,6 +75,21 @@ while game:
 
         ball.reset()
         ball.ball_update()
+
+        if ball.rect.x < 0:
+            score2 += 1
+            ball.rect.x = 300
+            ball.rect.y = 200
+
+        if ball.rect.x > 600:
+            score1 += 1
+            ball.rect.x = 300
+            ball.rect.y = 200
+        
+        text_score1 = font.render('Игрок 1 - ' + str(score1),True,(0,0,0))
+        window.blit(text_score1,(70,10))
+        text_score2 = font.render('Игрок 2 - ' + str(score2),True, (0,0,0))
+        window.blit(text_score2,(390,10))
 
     display.update()
     clock.tick(FPS)
